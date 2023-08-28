cf3343e1e3538839c449aeb7fa20384d7ec83432
RPB <sumitomo.andi@gmail.com>
2023-08-28 22:16:14 +0700

    feat: modify endpoint

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -3,11 +3,11 @@ const userRoutes = express.Router()
 import userController from '../controllers/user.controller'
 
 
-userRoutes.get('/v1/user', userController.getAllUsers)
-userRoutes.get('/v1/user/:id', userController.getOneUser)
+userRoutes.get('/v1/users', userController.getAllUsers)
+userRoutes.get('/v1/users/:id', userController.getOneUser)
 userRoutes.post('/v1/auth/register', userController.regUser)
 userRoutes.post('/v1/auth/login', userController.loginUser)
-userRoutes.delete('/v1/user/:id', userController.deleteUser)
-userRoutes.patch('/v1/user/:id', userController.updateUser)
+userRoutes.delete('/v1/users/:id', userController.deleteUser)
+userRoutes.patch('/v1/users/:id', userController.updateUser)
 
 export default userRoutes
